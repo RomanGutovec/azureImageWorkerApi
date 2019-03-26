0e24a359738c0f65cb9c5b4bc63c737dba493497
Roman Gutovec <roman.gutovec@gmail.com>
2019-03-26 18:06:12 +0300

    Remove unnecessary code. Change Nlog config.

--- ImagesWorkerAPI/Controllers/ImagesController.cs
+++ ImagesWorkerAPI/Controllers/ImagesController.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 using ImagesWorkerAPI.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using NLog;
 
 namespace ImagesWorkerAPI.Controllers
